83d6b2be2a070c723b405805c0ba7844aa0efd00
Cindy Batbayar <cindybatbayar@Cindys-MacBook-Pro.local>
2023-02-02 21:11:26 -0600

    aaaaa

--- src/App.js
+++ src/App.js
@@ -2,14 +2,13 @@ import './App.css';
 
 function App() {
   return (<>
-  <div>Muchirnii navch goydoo</div>
-  <div>Muchuruu bugdeeree togloo</div>
-
-  <div>Muugii muchiruur togloh durtai</div>
-
-  <div>Muugii uuruu muchir yum</div>
+  <div> Muchirnii navch goydoo </div>
+  <div> Muchuruu bugdeeree togloo </div>
+  <div> Muugii muchiruur togloh durtai </div>
+  <div> Muugii uuruu muchir yum </div>
   <div> Namriin nalgar udur ch saihan shu </div>
   <div> Nasnii namaraaa gadaaa ireheeeed </div>
+  
   
   
   </>
