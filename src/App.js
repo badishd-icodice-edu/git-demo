2233e391583f09b8135a74ef7365e8b95b98bf51
gankhulugdev <ihcrew_hulguu@yahoo.com>
2023-02-02 21:09:19 -0600

    4r heseg

--- src/App.js
+++ src/App.js
@@ -4,8 +4,10 @@ function App() {
   return (<>
   <div>Muchirnii navch goydoo</div>
   <div>Muchuruu bugdeeree togloo</div>
-
+  <div>Muchir bol hair yum</div>
   <div>Muugii muchiruur togloh durtai</div>
+  
+
   
   </>
     
