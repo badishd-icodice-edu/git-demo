4be536c89d5a42c863377d68daf6869045753b9b
gankhulugdev <ihcrew_hulguu@yahoo.com>
2023-02-02 21:14:01 -0600

    hairiin muchir

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ function App() {
   return (<>
   <div>Muchirnii navch goydoo</div>
   <div>Muchuruu bugdeeree togloo</div>
-
+  <div>Muchir bol hair yum</div>
   <div>Muugii muchiruur togloh durtai</div>
 
   <div>Muugii uuruu muchir yum</div>
